dda2d3a1931e1dc460e4b855839c69dec46b2474
Maciej Koczot <mefiuea@gmail.com>
2022-09-05 11:23:09 +0200

    upgrade navbar. Add custom breakpoint for screens

--- pmef/src/components/Navbar.vue
+++ pmef/src/components/Navbar.vue
@@ -10,25 +10,24 @@
                         <!-- logo -->
                             <a href="" class="flex items-center py-1">
                                 <img src="../assets/pme_icon.png" class="" width="50px" height="50px" alt="Logo">
-                                <!-- <img src="../assets/pme_text.png" class="logo mt-2" width="300px" height="100px" alt="text"> -->
-                                <p class="pme-logo px-4 text-lime-700">Parcel Management Environment</p>
+                                <p class="pme-logo px-4 text-lime-700 hidden sm:flex">Parcel Management Environment</p>
+                                <p class="pme-logo px-4 text-lime-700 hidden ss:flex sm:hidden">PME</p>
                             </a>
                         <!-- menu -->
                         <div class="hidden md:flex items-center px-2">
-                            <a href="/" class="py-2 px-4 hover:text-green-500 rounded-full font-bold">Home</a>
+                            <a href="/" class="py-2 px-4 hover:text-green-500 font-bold">Home</a>
                             <a href="/" class="py-0 px-4 hover:text-green-500">About</a>
                             <a href="/" class="py-0 px-4 hover:text-green-500">Contact</a>
                         </div>
                     </div>
                     <!-- login / register -->
                     <div class="flex items-center px-2 space-x-2" :class="{ hidden: !showMobileMenu }">
-                        <a href="/" class="py-2 px-4 rounded-full text-black font-bold border-2 border-green-500 hover:bg-green-500">Login</a>
-                        <a href="/" class="py-2 px-4 rounded-full text-black font-bold border-2 border-red-500 hover:bg-red-500">Register</a>
+                        <a href="/" class="duration-300 py-2 px-4 rounded-full text-black font-bold border-2 border-green-500 hover:bg-green-500">Login</a>
+                        <a href="/" class="duration-300 py-2 px-4 rounded-full text-black font-bold border-2 border-red-500 hover:bg-red-500">Register</a>
                     </div>
                     <!-- button show mobile menu -->
                     <div class="md:hidden flex items-center">
                         <button @click="showMobileMenu = !showMobileMenu" class="py-0 px-2">
-                            <!-- <img src="../assets/dropdown_icon.png" width="25px" height="25px" alt="Drop down menu"> -->
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
                             </svg>
@@ -39,12 +38,12 @@
         </div>
         <!-- Mobile Menu -->
         <div class="md:hidden border-b-2 border-lime-700" :class="{ hidden: showMobileMenu }">
-            <a href="/" class="block py-2 px-4 hover:bg-green-500">Home</a>
-            <a href="/" class="block py-2 px-4 hover:bg-green-500">About</a>
-            <a href="/" class="block py-2 px-4 hover:bg-green-500">Contact</a>
+            <a href="/" class="duration-300 block py-2 px-4 hover:bg-green-500 font-bold">Home</a>
+            <a href="/" class="duration-300 block py-2 px-4 hover:bg-green-500">About</a>
+            <a href="/" class="duration-300 block py-2 px-4 hover:bg-green-500">Contact</a>
             <div class="py-4 space-x-2">
-                <a href="/" class="py-2 px-4 rounded-full text-black font-bold border-2 border-green-500 hover:bg-green-500">Login</a>
-                <a href="/" class="py-2 px-4 rounded-full text-black font-bold border-2 border-red-500 hover:bg-red-500">Register</a>
+                <a href="/" class="duration-300 py-2 px-4 rounded-full text-black font-bold border-2 border-green-500 hover:bg-green-500">Login</a>
+                <a href="/" class="duration-300 py-2 px-4 rounded-full text-black font-bold border-2 border-red-500 hover:bg-red-500">Register</a>
             </div>
         </div>
     </nav>
--- pmef/tailwind.config.js
+++ pmef/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'ss': '347px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
