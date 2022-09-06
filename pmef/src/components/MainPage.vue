f082648935d64fa0225477565d90abbd8c8ac1cb
Maciej Koczot <mefiuea@gmail.com>
2022-09-06 14:26:12 +0200

    upgrade main page. Organizing files.

--- pmef/src/components/MainPage.vue
+++ pmef/src/components/MainPage.vue
@@ -1,65 +1,80 @@
 <template>
   <div>
-    <p>Test</p>
-    <button
+    <div
       class="
-        bg-blue-500
-        hover:bg-blue-400
-        text-white
-        font-bold
-        py-2
-        px-4
-        border-b-4 border-blue-700
-        hover:border-blue-500
-        rounded
+        max-w-screen-lg
+        bg-white
+        rounded-xl
+        border-lime-700 border
+        overflow-hidden
+        mx-4
       "
-      v-on:click="increment()"
     >
-      Increment++
-    </button>
-
-    <button
-      class="
-        bg-red-500
-        hover:bg-red-400
-        text-white
-        font-bold
-        py-2
-        px-4
-        border-b-4 border-red-700
-        hover:border-red-500
-        rounded
-      "
-      v-on:click="decrement()"
-    >
-      Decrement--
-    </button>
-    <div class="m-6">
-      <p class="text-9xl">Counter status: {{ counter }}</p>
+      <div class="md:flex">
+        <div class="md:shrink-0 px-4 py-4">
+          <img
+            class="h-48 w-full object-contain md:h-full md:w-48"
+            src="../assets/images/question_circle_icon.png"
+            alt="Man looking at item at a store"
+          />
+        </div>
+        <div class="p-8 grow">
+          <div
+            class="uppercase tracking-wide text-4xl font-semibold text-center"
+          >
+            What is it about?
+          </div>
+          <div class="mt-4 text-justify">
+            <span class="pme-logo text-lime-700">Parcel Management System</span>
+            is a system for managing parcels in your home. It allows you to
+            create virtual parcels that can be combined with real parcels, so
+            you can stay organized. Please see below how you can use the system
+            in a few simple steps, thanks to which you will no longer have to
+            remember where your items are.
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
-import { onMounted, onUpdated, ref } from "vue";
+<script>
+import { onMounted, onUpdated, ref, toRefs } from "vue";
 
-// variables
-const counter = ref(2022);
+export default {
+  props: {},
 
-// functions
-function increment() {
-  counter.value++;
-  console.log(`Counter value = ${counter.value}.`);
-}
+  setup(props) {
+    // variables
+    const counter = ref(2022);
+    const testFunction = test();
 
-function decrement() {
-  counter.value--;
-  console.log(`Counter value = ${counter.value}.`);
-}
+    // functions
+    function increment() {
+      counter.value++;
+      console.log(`Counter value = ${counter.value}.`);
+    }
 
-// mounted
-onMounted(() => {
-  console.log(`Counter value = ${counter.value}.`);
-});
+    function decrement() {
+      counter.value--;
+      console.log(`Counter value = ${counter.value}.`);
+    }
 
+    function test() {
+      console.log("TEST");
+    }
+
+    return {
+      counter,
+      increment,
+      decrement,
+      testFunction,
+    };
+  },
+
+  // mounted
+  // mounted() {
+  //   console.log(`Counter value = ${counter.value}.`);
+  // },
+};
 </script>
