c2e2b68e93c541173fd30a4ab138a450fadf95a3
Maciej Koczot <mefiuea@gmail.com>
2022-09-08 18:51:18 +0200

    upgrade register page

--- pmef/src/components/RegisterPage.vue
+++ pmef/src/components/RegisterPage.vue
@@ -1,6 +1,109 @@
 <template>
   <!-- root -->
   <div>
+    <div class="min-h-screen flex flex-col">
+      <div
+        class="
+          container
+          max-w-sm
+          mx-auto
+          flex-1 flex flex-col
+          items-center
+          justify-center
+          px-2
+        "
+      >
+        <div
+          class="
+            bg-white
+            px-6
+            py-8
+            border-lime-700 border
+            rounded-xl
+            text-black
+            w-full
+          "
+        >
+          <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+          <input
+            type="text"
+            class="
+              block
+              border border-lime-700
+              w-full
+              p-3
+              rounded-xl
+              mb-4
+              hover:bg-gray-100
+            "
+            name="email"
+            placeholder="Email"
+          />
+
+          <input
+            type="password"
+            class="
+              block
+              border border-lime-700
+              w-full
+              p-3
+              rounded-xl
+              mb-4
+              hover:bg-gray-100
+            "
+            name="password"
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            class="
+              block
+              border border-lime-700
+              w-full
+              p-3
+              rounded-xl
+              mb-4
+              hover:bg-gray-100
+            "
+            name="confirm_password"
+            placeholder="Confirm Password"
+          />
+
+          <button
+            type="submit"
+            name="create_account_button"
+            value="clicked"
+            class="
+              w-full
+              text-center
+              hover:bg-green-dark
+              focus:outline-none
+              my-1
+              duration-300
+              py-2
+              px-4
+              rounded-full
+              text-black
+              font-bold
+              border-2 border-red-500
+              hover:bg-red-500
+            "
+          >
+            Create Account
+          </button>
+
+          <div class="text-center text-sm text-grey-dark mt-4">
+            Already have an account?
+            <a
+              class="border-b border-green-500 text-green-500"
+              href="/"
+            >
+              Log in</a
+            >.
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- root end -->
   </div>
 </template>
