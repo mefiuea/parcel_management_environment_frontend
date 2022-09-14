a99ae9ce2a52efafc3de72692ad92dd268a0ab06
Maciej Koczot <mefiuea@gmail.com>
2022-09-14 17:20:59 +0200

    install pinia

--- pmef/src/axios/axios-api.js
+++ pmef/src/axios/axios-api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const getAPI = axios.create({
   baseURL: "http://127.0.0.1:8000",
   timeout: 8000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
 });
 
 export { getAPI };
