c6de2f29b337ef1b26de9c7f5f206ea23f17581e
Maciej Koczot <mefiuea@gmail.com>
2022-09-14 10:44:47 +0200

    upgrade logic and validation in register page

--- pmef/src/axios/axios-api.js
+++ pmef/src/axios/axios-api.js
@@ -1,8 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
 const getAPI = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
-    timeout: 1000,
-})
+  baseURL: "http://127.0.0.1:8000",
+  timeout: 8000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export { getAPI }
+export { getAPI };
--- pmef/src/components/RegisterPage.vue
+++ pmef/src/components/RegisterPage.vue
@@ -10,9 +10,9 @@
         >
           <h1 class="mb-8 text-3xl text-center">Sign up</h1>
           <form action="http://localhost:5173/" method="get" @submit.prevent>
-            <div v-if="APIData.email" class="text-sm text-red-500">
+            <div v-if="APIDataError.email" class="text-sm text-red-500">
               Email:
-              <li v-for="item in APIData.email" v-bind:key="item">
+              <li v-for="item in APIDataError.email" v-bind:key="item">
                 {{ item }}
               </li>
             </div>
@@ -26,20 +26,23 @@
 
             <div
               :class="{
-                'border-2': APIData.non_field_errors,
-                'border-dashed': APIData.non_field_errors,
-                'border-red-500': APIData.non_field_errors,
+                'border-2': APIDataError.non_field_errors,
+                'border-dashed': APIDataError.non_field_errors,
+                'border-red-500': APIDataError.non_field_errors,
               }"
             >
               <!-- class="border-2 border-dashed border-red-500" -->
-              <div v-if="APIData.password1" class="text-sm text-red-500">
+              <div v-if="APIDataError.password1" class="text-sm text-red-500">
                 Password:
-                <li v-for="item in APIData.password1" v-bind:key="item">
+                <li v-for="item in APIDataError.password1" v-bind:key="item">
                   {{ item }}
                 </li>
               </div>
-              <div v-if="APIData.non_field_errors" class="text-sm text-red-500">
-                {{ APIData.non_field_errors[0] }}
+              <div
+                v-if="APIDataError.non_field_errors"
+                class="text-sm text-red-500"
+              >
+                {{ APIDataError.non_field_errors[0] }}
               </div>
               <input
                 type="password"
@@ -49,9 +52,9 @@
                 v-model="submitForm.password1"
               />
 
-              <div v-if="APIData.password2" class="text-sm text-red-500">
+              <div v-if="APIDataError.password2" class="text-sm text-red-500">
                 Confirm password:
-                <li v-for="item in APIData.password2" v-bind:key="item">
+                <li v-for="item in APIDataError.password2" v-bind:key="item">
                   {{ item }}
                 </li>
               </div>
@@ -64,9 +67,9 @@
               />
             </div>
 
-            <div v-if="APIData.user_name" class="text-sm text-red-500">
+            <div v-if="APIDataError.user_name" class="text-sm text-red-500">
               User name:
-              <li v-for="item in APIData.user_name" v-bind:key="item">
+              <li v-for="item in APIDataError.user_name" v-bind:key="item">
                 {{ item }}
               </li>
             </div>
@@ -78,9 +81,9 @@
               v-model="submitForm.user_name"
             />
 
-            <div v-if="APIData.first_name" class="text-sm text-red-500">
+            <div v-if="APIDataError.first_name" class="text-sm text-red-500">
               First name:
-              <li v-for="item in APIData.first_name" v-bind:key="item">
+              <li v-for="item in APIDataError.first_name" v-bind:key="item">
                 {{ item }}
               </li>
             </div>
@@ -92,9 +95,9 @@
               v-model="submitForm.first_name"
             />
 
-            <div v-if="APIData.last_name" class="text-sm text-red-500">
+            <div v-if="APIDataError.last_name" class="text-sm text-red-500">
               Last name:
-              <li v-for="item in APIData.last_name" v-bind:key="item">
+              <li v-for="item in APIDataError.last_name" v-bind:key="item">
                 {{ item }}
               </li>
             </div>
@@ -106,7 +109,9 @@
               v-model="submitForm.last_name"
             />
 
+            <!-- Submit button if -->
             <button
+              v-if="!loading"
               type="submit"
               name="create_account_button"
               value="clicked"
@@ -115,6 +120,29 @@
             >
               Create Account
             </button>
+            <!-- else -->
+            <div v-else class="text-center">
+              <div role="status">
+                <svg
+                  class="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <div>
+                  <span class="">Loading...</span>
+                </div>
+              </div>
+            </div>
           </form>
 
           <div class="text-center text-sm text-grey-dark mt-4">
@@ -123,10 +151,6 @@
               Log in</a
             >.
           </div>
-        </div>
-        Odpowiedź:
-        <div>
-          {{ APIDataSuccess }}
         </div>
       </div>
     </div>
@@ -137,13 +161,14 @@
 <script>
 import { onMounted, onUpdated, ref, toRefs } from "vue";
 import { getAPI } from "../axios/axios-api.js";
+import { useRouter } from "vue-router";
 
 export default {
   props: {},
 
   setup(props) {
-    const APIData = ref([]);
-    const APIDataSuccess = ref('pusto');
+    const APIDataError = ref("");
+    const APIDataSuccess = ref("");
     const submitForm = ref({
       email: "",
       password1: "",
@@ -152,31 +177,36 @@ export default {
       first_name: "",
       last_name: "",
     });
+    const loading = ref(false);
+    const router = useRouter();
 
     function getDataFromApi() {
-      console.log("test API");
+      loading.value = true;
       getAPI
         .post("/api/v1/dj-rest-auth/registration/", submitForm.value)
         .then((response) => {
-          console.log("Registration API has recived data");
+          loading.value = true;
           APIDataSuccess.value = response.data;
-          console.log("Data: ", APIDataSuccess.value);
-          console.log(`Status code: ${response.status}`);
+          router.push({
+            name: "MP",
+          });
         })
         .catch((err) => {
+          loading.value = false;
           console.log("Error response status:", err.response.status);
           console.log("Error message:", err.message);
           console.log("Error code:", err.code);
           console.log("Error:", err.response.data);
-          APIData.value = err.response.data;
+          APIDataError.value = err.response.data;
         });
     }
 
     return {
-      APIData,
+      APIDataError,
       APIDataSuccess,
       getDataFromApi,
       submitForm,
+      loading,
     };
   },
 };
