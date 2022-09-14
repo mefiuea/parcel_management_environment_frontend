550f8a36f08475b4afba4241b9208acfbbf2f9d2
Maciej Koczot <mefiuea@gmail.com>
2022-09-14 17:26:32 +0200

    setup pinia in main.js

--- pmef/src/main.js
+++ pmef/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 
 import "./index.css";
 import "./assets/styles/main.css";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia()
+
+createApp(App).use(router, pinia).mount("#app");
